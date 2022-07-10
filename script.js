fcd7404dd965aab81f31c3005f1fc1206300bead
MrKubul <jakub.dryka1@gmail.com>
2022-07-11 00:10:43 +0200

    working, but not centered full three modals

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function togglePopup() {
+function togglePopup1() {
     document.getElementById("popup1").classList.toggle("active")
+}
+function togglePopup2() {
+    document.getElementById("popup2").classList.toggle("active")
+}
+function togglePopup3() {
+    document.getElementById("popup3").classList.toggle("active")
 }
